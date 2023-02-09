ac61facacdb2fc9d495e6f93c6888e24e012945f
Humayun Khan <humayunk.pvt@gmail.com>
2023-02-09 16:26:40 +0530

    init

--- script.js
+++ script.js
@@ -11,11 +11,47 @@ yearEl.innerHTML = new Date().getFullYear();
 /* *********** */
 const projects = [
   {
-    name: 'Calculator App',
-    img: '4.png',
-    codeUrl: 'https://github.com/justdecodeme/js-practice-01-change-bg',
-    liveUrl: 'https://justdecodeme.github.io/js-practice-01-change-bg/'
+    name: 'Favourite Food App',
+    img: '1.png',
+    codeUrl: 'https://github.com/HumayunK01/foodapp',
+    liveUrl: 'https://humayunk01.github.io/foodapp/'
   },
+  {
+    name: 'EdYoda Stories',
+    img: '2.png',
+    codeUrl: 'https://github.com/HumayunK01/edyodastories2.0',
+    liveUrl: 'https://humayunk01.github.io/edyodastories2.0/'
+  },
+  {
+    name: 'Stunning Landing Page',
+    img: '3.png',
+    codeUrl: 'https://github.com/HumayunK01/stunninglandingpage',
+    liveUrl: 'https://humayunk01.github.io/stunninglandingpage/'
+  },
+  {
+    name: 'Themes Switcher',
+    img: '4.png',
+    codeUrl: 'https://github.com/HumayunK01/jsassignment1',
+    liveUrl: 'https://humayunk01.github.io/jsassignment1/'
+  },
+  {
+    name: 'Dark to Light Mode',
+    img: '5.png',
+    codeUrl: 'https://github.com/HumayunK01/jshomework2',
+    liveUrl: 'https://humayunk01.github.io/jshomework2/'
+  },
+  {
+    name: 'Color Generator',
+    img: '6.png',
+    codeUrl: 'https://github.com/HumayunK01/colorgenerator',
+    liveUrl: 'https://humayunk01.github.io/colorgenerator/'
+  },
+  {
+    name: 'Table Generator',
+    img: '7.png',
+    codeUrl: 'https://github.com/HumayunK01/tablegenerator',
+    liveUrl: 'https://humayunk01.github.io/tablegenerator/'
+  }
 ]
 
 let output = '';
@@ -27,7 +63,7 @@ for (let i = 0; i < projects.length; i++) {
     <p>${projects[i].name}</p>
     <p class="flex">
       <a href="${projects[i].codeUrl}" target="_blank"><i class="icon ri-github-fill"></i></a>
-      <a href="${projects[i].liveUrl}" target="_blank"><i class="icon ri-link"></i></a>
+      <a href="${projects[i].liveUrl}" target="_blank"><i class="icon ri-external-link-line"></i></a>
     </p>
   </div>
 </div> `
